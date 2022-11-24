ecde43eba4aea6c77f415c5aab41994babcd31a8
lyonchikk1 <alena.karpushina2015@yandex.ru>
2022-11-24 21:35:53 +0300

    add function find_student

--- main.py
+++ main.py
@@ -37,3 +37,12 @@ def add_student(student: list, last_name: str, first_name: str, klass: str) -> N
             "klass": klass
         }
     )
+
+
+def find_student(student: list, last_name: str, first_name: str) -> dict:
+    stud = next((stud
+                for stud in student
+                if stud.get("last_name") == last_name
+                and stud.get("first_name") == first_name), None)
+    return stud
+
